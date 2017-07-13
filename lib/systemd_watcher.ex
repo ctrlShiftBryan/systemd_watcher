b55dea69d548d10485458cb584b5982439000952
Bryan Arendt <barendt@covermymeds.com>
2017-07-12 21:49:55 -0400

    create parser

--- lib/systemd_watcher.ex
+++ lib/systemd_watcher.ex
@@ -1,2 +1,52 @@
 defmodule SystemdWatcher do
+  def sample do
+    input = """
+    Jul 12 20:09:37 mine ethminer[32294]:   m  20:09:37|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:38 mine ethminer[32294]:   m  20:09:38|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:38 mine ethminer[32294]:   m  20:09:38|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:38 mine ethminer[32294]:   m  20:09:38|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:38 mine ethminer[32294]:   m  20:09:38|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:38 mine ethminer[32294]:   m  20:09:38|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:39 mine ethminer[32294]:   m  20:09:39|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:39 mine ethminer[32294]:   m  20:09:39|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:39 mine ethminer[32294]:   m  20:09:39|ethminer  Mining on PoWhash #1029edbe : 26.21MH/s [A0+0:R0+0:F0]
+    """
+  end
+
+  def parse_log(text) do
+    %{text: text}
+  end
+
+  @pid_regex ~r/(?<=ethminer\[)(.+)(?=\]:)/
+
+  def get_all_pids(input) do
+    @pid_regex |> Regex.scan(input) |> fix_nil |> List.flatten |> Enum.uniq
+  end
+
+
+  def get_info(input) do
+    with x when x in ["",nil]<- @pid_regex
+                                |> Regex.run(input)
+                                |> fix_nil
+                                |> List.flatten
+                                |> Enum.at(0)
+    do
+      :not_found
+    else
+      pid ->
+        with {m, _} <- ~r/(?<=\ \:\ )(.+)(?=MH\/s)/
+                      |> Regex.run(input)
+                      |> fix_nil
+                      |> List.flatten
+                      |> Enum.at(0)
+                      |> Float.parse do
+          {:ok, pid, m}
+        else
+          _ -> {:ok, pid, 0.0}
+        end
+    end
+  end
+
+  defp fix_nil(nil), do: []
+  defp fix_nil(not_nil), do: not_nil
 end
--- test/systemd_watcher_test.exs
+++ test/systemd_watcher_test.exs
@@ -2,7 +2,31 @@ defmodule SystemdWatcherTest do
   use ExUnit.Case
   doctest SystemdWatcher
 
-  test "the truth" do
-    assert 1 + 1 == 2
+
+
+  @input  "Jul 12 20:09:37 mine ethminer[32294]:   m  20:09:37|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]"
+  @input2 "Jul 12 20:09:37 mine ethmine[32294]:   m  20:09:37|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]"
+  @input3 """
+    Jul 12 20:09:37 mine ethminer[0]:   m  20:09:37|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:38 mine ethminer[1]:   m  20:09:38|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:38 mine ethminer[2]:   m  20:09:38|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:38 mine ethminer[3]:   m  20:09:38|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:38 mine ethminer[4]:   m  20:09:38|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:38 mine ethminer[5]:   m  20:09:38|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:39 mine ethminer[5]:   m  20:09:39|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:39 mine ethminer[5]:   m  20:09:39|ethminer  Mining on PoWhash #1029edbe : 31.46MH/s [A0+0:R0+0:F0]
+    Jul 12 20:09:39 mine ethminer[5]:   m  20:09:39|ethminer  Mining on PoWhash #1029edbe : 26.21MH/s [A0+0:R0+0:F0]
+    """
+
+  test "get pids" do
+    assert SystemdWatcher.get_info(@input) == {:ok, "32294",31.46}
+  end
+
+  test "not found" do
+    assert SystemdWatcher.get_info(@input2) == :not_found
+  end
+
+  test "get all pids" do
+    assert SystemdWatcher.get_all_pids(@input3) == ["0","1","2","3","4","5"]
   end
 end
