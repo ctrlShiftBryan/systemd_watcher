8aeb32667ab4338ac0cfc41fa4e62b1ebc3cdfe3
Bryan Arendt <barendt@covermymeds.com>
2017-07-13 22:05:11 -0400

    formatting

--- lib/systemd_watcher_gen_server.ex
+++ lib/systemd_watcher_gen_server.ex
@@ -16,14 +16,24 @@ defmodule SystemdWatcher.GenServer do
 
   def handle_cast({:add_pid, pid}, state) do
     with nil <- state[:pids][pid] do
-      {:noreply, state |> Map.merge(%{pids: %{pid => %{mhs: 0, time: System.monotonic_time}}})}
+      {:noreply, state |> Map.merge(
+        %{pids:
+          %{pid =>
+            %{mhs: 0,
+              time: System.monotonic_time}
+          }
+        }
+      )}
     else
       _ -> {:noreply, state}
     end
   end
 
   defp too_old(time, state) do
-    System.monotonic_time |> Kernel.-(time) |> Kernel./(1_000_000_000) |> Kernel.>(state[:timeout])
+    System.monotonic_time
+    |> Kernel.-(time)
+    |> Kernel./(1_000_000_000)
+    |> Kernel.>(state[:timeout])
   end
 
   def handle_call({:get_pid, pid}, _from, state) do
